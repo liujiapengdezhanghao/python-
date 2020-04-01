34069836f40afa0994d673983ca958e20ad80fd0
刘家朋 <62976782+carlliu-070616@users.noreply.github.com>
2020-04-01 16:13:58 +0800

    Update app.py

--- demos/hello/app.py
+++ demos/hello/app.py
@@ -1,5 +1,18 @@
 """
-
+请将此文件夹和pipfile pipfile.lock复制到本地
+然后进入文件目录：打开cmd
+输入：
+pip install pipenv
+pipenv install
+pipenv shell
+flask run
+现在使用浏览器打开“http://127.0.0.1:5000/hello/你的名字”即可体验
+下次开启只需在文件目录cmd输入：
+pipenv shell
+flask run
+若要部署上线，请搜索Pythonanywhere
+更多资料请见《Flask Web开发实战 入门进阶和原理解析》
+刘家朋 2020/4/1
 """
 from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
